ee0be40108a1e8371fc26f1aee9d703c1548a8ab
Yuriy Zaytsev <y.zaytsev@netris.ru>
2023-02-15 12:34:56 +0300

    configuration: добавлен пакет для базовой работы с конфигами

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a library which contains common packages of all RacoonMediaServer microservices.
 
 ## Important packages
+
+* **configuration** - generalized code for loading configuration files
---
+++ pkg/configuration/configuration.go
@@ -0,0 +1,45 @@
+package configuration
+
+import (
+	"fmt"
+	"go-micro.dev/v4/config"
+	"go-micro.dev/v4/config/source/env"
+	"go-micro.dev/v4/config/source/file"
+	"go-micro.dev/v4/logger"
+)
+
+// Database is required fields for connecting to database
+type Database struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Database string
+}
+
+// Load read and parses configuration file. Also override some fields via environment variables
+// Ex. you can set environment variable RMS_DATABASE_USER which override value root / database / user
+func Load(configFile string, configuration interface{}) error {
+	err := config.Load(
+		file.NewSource(file.WithPath(configFile)),
+		env.NewSource(env.WithStrippedPrefix("RMS")),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	if err := config.Scan(&configuration); err != nil {
+		return err
+	}
+
+	logger.Infof("Configuration: %+v", configuration)
+
+	return nil
+}
+
+// GetConnectionString composes PostgresSQL connection string to RMS database
+func (config Database) GetConnectionString() string {
+	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		config.Host, config.Port, config.User, config.Password, config.Database)
+}
