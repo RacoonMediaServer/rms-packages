fbe65a913f3fc807b823f40852bba73b97225008
Yuriy Zaytsev <y.zaytsev@netris.ru>
2023-02-16 11:52:44 +0300

    api: добавлено API к серверной части бота

--- pkg/service/servicemgr/manager.go
+++ pkg/service/servicemgr/manager.go
@@ -1,11 +1,13 @@
 package servicemgr
 
 import (
+	rms_bot_server "github.com/RacoonMediaServer/rms-packages/pkg/service/rms-bot-server"
 	rms_users "github.com/RacoonMediaServer/rms-packages/pkg/service/rms-users"
 	"go-micro.dev/v4/client"
 )
 
 //go:generate ../rms-users/gen.sh
+//go:generate ../rms-bot-server/gen.sh
 
 // ClientFactory can spawn microservices clients
 type ClientFactory interface {
@@ -25,4 +27,9 @@ func NewServiceFactory(f ClientFactory) ServiceFactory {
 // NewUsers create connection to rms-users service
 func (f ServiceFactory) NewUsers() rms_users.RmsUsersService {
 	return rms_users.NewRmsUsersService("rms-users", f.constructor.Client())
+}
+
+// NewBotServer creates connection to rms-bot-server service
+func (f ServiceFactory) NewBotServer() rms_bot_server.RmsBotServerService {
+	return rms_bot_server.NewRmsBotServerService("rms-bot-server", f.constructor.Client())
 }
