62c4ac7eb0b456b14fe8eb304a0a84b6d4e2a64b
Yuriy Zaytsev <y.zaytsev@netris.ru>
2023-02-16 12:57:52 +0300

    communication: в самом начальном виде созданы структуры сообщений между клиентом и сервером бота

---
+++ pkg/communication/botmessage.go
@@ -0,0 +1,6 @@
+package communication
+
+// BotMessage represents a message from Telegram bot client to user
+type BotMessage struct {
+	Text string
+}
---
+++ pkg/communication/usermessage.go
@@ -0,0 +1,6 @@
+package communication
+
+// UserMessage represents a Telegram message from user to device
+type UserMessage struct {
+	Text string
+}
